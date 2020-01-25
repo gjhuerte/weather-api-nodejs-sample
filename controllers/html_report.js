c671e8695546607d36e376be2e7268e3b385b4b5
Gabriel Jay Huerte <gjhuerte@gmail.com>
2020-01-25 15:08:16 +0800

    Added instructions to use the files

--- controllers/html_report.js
+++ controllers/html_report.js
@@ -1,1 +1,60 @@
 const fs = require('fs');
+const axios = require('axios');
+const moment = require('moment');
+
+const initialize = async (res) => {
+    const timestamp = moment().format('MMDDYYHHmmss');
+
+    try {
+        await axios.get('https://samples.openweathermap.org/data/2.5/weather?', {
+            params: {
+                q: 'London,uk',
+                appid: 'b6907d289e10d714a6e88b30761fae22',
+            }
+        }).then(response => {
+            const path = `./reports/weather-${timestamp}.txt`;
+            const responseData = response.data;
+            const params = {
+                weatherType: responseData.weather[0].main,
+                locationName: responseData.name,
+                locationCode: responseData.sys.country,
+                locationIcon:  `http://openweathermap.org/images/flags/${responseData.sys.country.toLowerCase()}.png`,
+                temperature: responseData.main.temp - 273.15,
+                temperatureMinimum: responseData.main.temp_min - 273.15,
+                temperatureMaximum: responseData.main.temp_max - 273.15,
+                longitude: responseData.coord.lon,
+                latitude: responseData.coord.lat,
+                windSpeed: responseData.wind.speed,
+                hpa: responseData.main.pressure,
+                clouds: responseData.clouds.all,
+            };
+
+            res.setHeader('Content-Type', 'text/html');
+            fs.readFile('./views/report.html', (error, data) => {
+                const html = reconstructContent(data, params);
+                res.end(html);
+            });
+        }).catch (error => {
+            res.statusCode = 500;
+            res.end(JSON.stringify(error));
+        });
+    } catch (error) {
+        res.statusCode = 500;
+        res.end(JSON.stringify(error));
+    }
+};
+
+function reconstructContent(data, params) {
+    let _data = data.toString();
+
+    for (var key in params) {
+        let queryString = `{{ ${key} }}`;
+        if (_data.includes(queryString)) {
+            _data = _data.replace(queryString, params[key]);
+        }
+    }
+
+    return _data;
+}
+
+module.exports = initialize;
---
+++ readme.md
@@ -0,0 +1,33 @@
+### Weather Application
+
+---
+
+Installation:
+
+Please run the ff to install the dependencies
+
+```
+npm install
+```
+
+To start the application, you need to run the nodejs server by running the following
+
+
+```
+node main.js
+```
+
+
+
+There are two accessible routes in the application:
+
+
+> http://127.0.0.1:8080/
+
+You can create the txt report files in this route. The report fetches data from openweathermap
+
+> http://127.0.0.1:8080/html_report
+
+Show the report as an html format
+
+
